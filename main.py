3569e600526c23ca90b33c92ff3eea1d7c085c6c
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2026-01-24 22:39:21 -0700

    stubbing ideas out

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
+from typing import Dict, Mapping
+
+# from enum import StrEnum, auto
+
+# class _Style(StrEnum):
+#     BUILTIN = auto()
+#     ORJSON = auto()
+
+
 try:
-    import osjson as json  # pyright: ignore[reportMissingImports]
+    import orjson as json  # pyright: ignore[reportMissingImports]
+
+    print("using orjson")
+    # _style = _Style.ORJSON
 except ImportError:
     import json
 
+    print("using builtin json")
+    # _style = _Style.BUILTIN
+
+
+class pyddb:
+    def __init__(self, filename: str | None) -> None:
+        if not filename:
+            self.file = "db.json"
+            return
+        self.file = filename
+
+        try:
+            with open(self.file, "r") as f:
+                self.data = json.load(f)
+        except FileNotFoundError:
+            self.data = {}
+
+    def add(self, data: Dict | Mapping) -> None:
+        self.data.update(data)
+
+    def update(self, data: Dict | Mapping) -> None:
+        self.data.update(data)
+
+    def save(self) -> None:
+        try:
+            with open(self.file, "w") as f:
+                f.write(json.dumps(self.data, ensure_ascii=False))
+        except Exception as e:
+            print(f"error saving data to file: {e}")
+
 
 def main():
-    try:
-        with open("data.json", "r") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
     print("Hello from pyddb!")
 
 
